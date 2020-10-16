d5cf6f317f98239d7863d64f0030aaa554415e1a
Keyno <kofujiw@gmail.com>
2020-10-16 23:49:39 +0900

    commit. integration.

--- core/src/main/scala/caliban/GraphQL.scala
+++ core/src/main/scala/caliban/GraphQL.scala
@@ -1,5 +1,16 @@
 package caliban
 
-class GraphQL {
+trait GraphQL[-R] {
+  self =>
 
+}
+
+object GraphQL {
+  def graphQL[R, Q, M, S: SubscriptionSchema](resolver: RootResolver[Q, M, S], directives: List[__Directive] = Nil)(
+        implicit querySchema: Schema[R, Q],
+             mutationSchema: Schema[R, M],
+        subscriptionSchema: Schema[R, S]
+  ): GraphQL[R] = new GraphQL[R] {
+
+  }
 }
