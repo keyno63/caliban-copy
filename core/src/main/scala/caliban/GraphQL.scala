87a47523d1d0e684434942afaa5e164a94bd4ec0
Keyno <kofujiw@gmail.com>
2020-10-18 07:53:01 +0900

    format GraphQL class.

--- core/src/main/scala/caliban/GraphQL.scala
+++ core/src/main/scala/caliban/GraphQL.scala
@@ -11,10 +11,10 @@ trait GraphQL[-R] { self =>
   final def render: String =
     s"""schema {
        |${schemaBuilder.query.flatMap(_.opType.name).fold("")(n => s" query: $n\n")}${schemaBuilder.mutation
-      .flatMap(_.opType.name)
-      .fold("")(n => s" mution: $n\n")}${schemaBuilder.subscription
-      .flatMap(_.opType.name)
-      .fold("")(n => s" subscription: $n\n")}}
+         .flatMap(_.opType.name)
+         .fold("")(n => s" mution: $n\n")}${schemaBuilder.subscription
+         .flatMap(_.opType.name)
+         .fold("")(n => s" subscription: $n\n")}}
        |
        |${renderTypes(schemaBuilder.types)}""".stripMargin
 
@@ -41,29 +41,29 @@ trait GraphQL[-R] { self =>
             additionalDirectives
           )
         lazy val introspectionRootSchema: RootSchema[Any] = Introspector.introspect(rootType)
-        lazy val introspectionRootType: RootType = RootType(introspectionRootSchema.query.opType, None, None)
+        lazy val introspectionRootType: RootType          = RootType(introspectionRootSchema.query.opType, None, None)
 
         new GraphQLInterpreter[R, CalibanError] {
           override def check(query: String): IO[CalibanError, Unit] =
             for {
-              document <- Parser.parseQuery(query)
-              intro = Introspector.isIntrospection(document)
+              document       <- Parser.parseQuery(query)
+              intro          = Introspector.isIntrospection(document)
               typeToValidate = if (intro) introspectionRootType else rootType
-              _ <- Validator.validate(document, typeToValidate)
+              _              <- Validator.validate(document, typeToValidate)
             } yield ()
 
           override def executeRequest(
-                                       request: GraphQLRequest,
-                                       skipValidation: Boolean,
-                                       enableIntrospection: Boolean
-                                     ): URIO[R, GraphQLResponse[CalibanError]] = ???
+            request: GraphQLRequest,
+            skipValidation: Boolean,
+            enableIntrospection: Boolean
+          ): URIO[R, GraphQLResponse[CalibanError]] = OperationType
         }
       }
 
   final def withWrapper[R2 <: R](wrapper: Wrapper[R2]): GraphQL[R2] =
     new GraphQL[R2] {
-      override protected val schemaBuilder: RootSchemaBuilder[R2] = self.schemaBuilder
-      override protected val wrappers: List[Wrapper[R2]] = wrapper :: self.wrappers
+      override protected val schemaBuilder: RootSchemaBuilder[R2]    = self.schemaBuilder
+      override protected val wrappers: List[Wrapper[R2]]             = wrapper :: self.wrappers
       override protected val additionalDirectives: List[__Directice] = self.additionalDirectives
     }
 
@@ -72,7 +72,7 @@ trait GraphQL[-R] { self =>
   final def combine[R1 <: R](that: GraphQL[R1]): GraphQL[R1] =
     new GraphQL[R1] {
       override protected val schemaBuilder: RootSchemaBuilder[R1] = self.schemaBuilder |+| that.schemaBuilder
-      override protected val wrappers: List[Wrapper[R1]] = self.wrappers ++ that.wrappers
+      override protected val wrappers: List[Wrapper[R1]]          = self.wrappers ++ that.wrappers
       override protected val additionalDirectives: List[__Directive] =
         self.additionalDirectives ++ that.additionalDirectives
     }
@@ -80,21 +80,21 @@ trait GraphQL[-R] { self =>
   final def |+|[R1 <: R](that: GraphQL[R1]): GraphQL[R1] = combine(that)
 
   final def rename(
-                  queriesName: Option[String] = None,
-                  mutationsName: Option[String] = None,
-                  subscriptionName: Option[String] = None
-                  ): GraphQL[R] = new GraphQL[R] {
-    override protected val schemaBuilder: RootSchemaBuilder[R] = ???
-    override protected val wrappers: List[Wrapper[R]] = self.wrappers
+    queriesName: Option[String] = None,
+    mutationsName: Option[String] = None,
+    subscriptionName: Option[String] = None
+  ): GraphQL[R] = new GraphQL[R] {
+    override protected val schemaBuilder: RootSchemaBuilder[R]     = ???
+    override protected val wrappers: List[Wrapper[R]]              = self.wrappers
     override protected val additionalDirectives: List[__Directive] = self.additionalDirectives
   }
 }
 
 object GraphQL {
   def graphQL[R, Q, M, S: SubscriptionSchema](resolver: RootResolver[Q, M, S], directives: List[__Directive] = Nil)(
-        implicit querySchema: Schema[R, Q],
-        mutationSchema: Schema[R, M],
-        subscriptionSchema: Schema[R, S]
+    implicit querySchema: Schema[R, Q],
+    mutationSchema: Schema[R, M],
+    subscriptionSchema: Schema[R, S]
   ): GraphQL[R] = new GraphQL[R] {
     val schemaBuilder: RootSchemaBuilder[R] = RootSchemaBuilder(
       resolver.queryResolver.map(r => Operation(querySchema.toType_(), querySchema.resolve(r))),
@@ -104,7 +104,7 @@ object GraphQL {
       )
     )
 
-    val wrappers: List[Wrapper[R]] = Nil
+    val wrappers: List[Wrapper[R]]              = Nil
     val additionalDirectives: List[__Directive] = directives
   }
 }
